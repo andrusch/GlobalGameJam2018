a4385f45848b18cfde6d494fc8a8bcd5303f7b65
TIMOTHY-PC\Timothy <timothy.s.andrus@gmail.com>
2018-01-27 15:02:05 -0600

    Adding working camera prefab.

--- GlobalGameJam2018/Assets/Scripts/PlayerScript.cs
+++ GlobalGameJam2018/Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-
+    public float moveSpeed;
 	// Use this for initialization
 	void Start () {
-		
+        moveSpeed = 1.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 v = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.Translate(v);
+        //transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime,0.0f);
+
+    }
 }
---
+++ GlobalGameJam2018/Assets/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class camerafollow : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - PlayerPrefab.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
+        //transform.LookAt(target);
+        transform.position = PlayerPrefab.transform.position + offset;
+    }
+}
