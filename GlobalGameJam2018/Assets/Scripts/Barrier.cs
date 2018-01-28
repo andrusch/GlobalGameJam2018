b3346d1c7dc30fd250ff8494b614b149cb0156dc
DESKTOP-KU0C1HU\M <kmbhm1@gmail.com>
2018-01-28 13:18:12 -0600

    Added destructable barrier to CellPrefab

---
+++ GlobalGameJam2018/Assets/Scripts/Barrier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour {
+
+    public int defensePoints = 10;
+    private int pointValAtDeath = 0;
+    private bool isDead = false;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (defensePoints <= pointValAtDeath && isDead == false)
+        {
+            Debug.Log("Barrier Destroyed!" + "; isDead = " + isDead); //Testing
+            isDead = true;
+            Destroy(gameObject);
+        }
+
+        //Testing
+        Debug.Log("Barrier Life = " + defensePoints.ToString() + "; isDead = " + isDead);
+        defensePoints -= 1;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collider Tag = " + collision.gameObject.tag); //Testing
+        if (collision.gameObject.tag == "Projectile")
+            defensePoints -= 1;
+    }
+
+}
--- GlobalGameJam2018/Assets/Scripts/Cell.cs
+++ GlobalGameJam2018/Assets/Scripts/Cell.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour {
 
-    public List<GameObject> Defenses = new List<GameObject>();
+    public GameObject DefenseBarrier;
     public bool IsNucleusDestroyed;
     public List<GameObject> PowerUps = new List<GameObject>();
 
+
     // Use this for initialization
-	void Start () {
-		
-	}
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
