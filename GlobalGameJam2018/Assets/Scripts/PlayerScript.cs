b143c232a7a1dca0a9b8a322b02fb39f4ebc4171
TIMOTHY-PC\Timothy <timothy.s.andrus@gmail.com>
2018-01-27 22:11:14 -0600

    full animations

--- GlobalGameJam2018/Assets/Scripts/PlayerScript.cs
+++ GlobalGameJam2018/Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,7 @@ public class PlayerScript : MonoBehaviour {
     public Vector3 b = new Vector3(1f,0f,0f);
     public float smoothSpeed = 20.0f;
     Animator animator;
+    public bool moving = GameState.Instance.CameraIsMoving;
 
     void Start () {
         animator= gameObject.GetComponent<Animator>();
@@ -17,14 +18,20 @@ public class PlayerScript : MonoBehaviour {
 	}
 
 	void Update () {
+        moving = GameState.Instance.CameraIsMoving;
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg+90;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-        if (GameState.Instance.CameraIsMoving)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
         {
             animator.SetBool("moving", true);
         }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("moving", false);
+        }
+        
         transform.position = Vector3.Lerp(camera.transform.position + offSet, camera.transform.position + offSet,smoothSpeed);
         
 
