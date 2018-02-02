2a11e86dd9c5aefb63a2ee0c23c0aa5fe5faca9b
DESKTOP-KU0C1HU\M <kmbhm1@gmail.com>
2018-02-01 19:00:05 -0600

    Created testing scene for projectile dev.

---
+++ GlobalGameJam2018/Assets/Scripts/PlayerScript_2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript_2 : MonoBehaviour {
+
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Horizontal");
+        float v = verticalSpeed * Input.GetAxis("Vertical");
+        transform.position = new Vector3(h, v, 0.0f);
+        transform.Translate(0, 0, Time.deltaTime);
+    }
+
+}
